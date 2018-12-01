61c8d58416cf18ce60278d141dd004c8ab786d8c
Ilja Dubinin <iliadubinin91@gmail.com>
2018-12-01 16:37:49 +0000

    refactoring

--- dead_link_reporter.go
+++ dead_link_reporter.go
@@ -14,23 +14,31 @@ func main() {
 	fmt.Println("Execution Started")
 	pathToFile := getPathToFIle()
 
-	fileHelper.DoOnEachLine(pathToFile,func(line string) {
-		_, parsingErr := url.ParseRequestURI(line)
-
-		if parsingErr != nil {
-			fmt.Printf("WARN: something is wrong with URL: %s\n", line)
-			return
-		}
-		_, getErr := http.Get(line)
-		if getErr != nil {
-			fmt.Printf("ERROR: Following link can't be opened: %s\nError:%v\n", line, getErr)
-		}
-	})
+	fileHelper.DoOnEachLine(pathToFile, checkLink)
 
 	fmt.Println("Execution finished")
 	// Wait for any key to prevent console closure
 	bufio.NewScanner(os.Stdin).Scan()
 
+}
+
+func checkLink(link string) {
+	if validLink(link) {
+		_, getErr := http.Get(link)
+		if getErr != nil {
+			fmt.Printf("ERROR: Following link can't be opened: %s\nError:%v\n", link, getErr)
+		}
+	}
+}
+
+func validLink(link string) bool {
+	_, parsingErr := url.ParseRequestURI(link)
+
+	if parsingErr != nil {
+		fmt.Printf("WARN: something is wrong with URL: %s\n", link)
+		return false
+	}
+	return true
 }
 
 func getPathToFIle() string {
